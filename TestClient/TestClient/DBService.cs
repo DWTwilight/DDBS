1bc6e0846a9ddf46ae835037d602fe31b5d20a58
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2021-03-31 10:34:25 +0800

    111

---
+++ TestClient/TestClient/DBService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace TestClient
+{
+    class DBService
+    {
+        private readonly HttpClient client;
+
+        public DBService()
+        {
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri("https://localhost:5001");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
